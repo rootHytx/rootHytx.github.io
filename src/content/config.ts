3bd2a860484b82b526b80a8bd02a22c4484f957c
hytx <up201906931@up.pt>
2026-01-28 17:30:47 +0000

    added sops-nix configuration page

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,18 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const utilities = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    category: z.enum(['system', 'docker', 'security', 'networking', 'general']),
+    category: z.enum([
+      "system",
+      "docker",
+      "security",
+      "networking",
+      "general",
+      "nix",
+    ]),
     tags: z.array(z.string()).optional(),
     created: z.date().optional(),
     updated: z.date().optional(),
